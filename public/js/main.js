c68050bb5859c11e0b867affcda5a833349b170f
Yuran Simao <yuransimao7@gmail.com>
2022-12-13 19:48:22 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+// f = farheit => (farheit-32) * 5/9  ;
+//c = celsius => celsius * 9/5 + 32  ;
+
 let Button = document.getElementById("Btn");
 let input = document.getElementById("Input")
 let viewfinder = document.getElementById("viewfinder")
@@ -12,12 +15,23 @@ Button.addEventListener("click",conversion)
 
 
 function conversion(){
-   const celsiusExist = DateInput.toUpperCase().includes('C')
-   const farhrenheit = DateInput.toUpperCase().includes('F')
+   let celsiusExist = DateInput.toUpperCase().includes('C')
+   let farhrenheit = DateInput.toUpperCase().includes('F')
 
    if(!celsiusExist && !farhrenheit){
     throw new Error('Verifique o grau')
    }
+
+   let degreeu = Number(DateInput.toUpperCase().replace("F", ""))
+   let formula =  farheit => (farheit-32) * 5/9  ;
+   let degreuSign = "C"
+
+   if(celsiusExist){
+     degreeu = Number(DateInput.toUpperCase().replace("C", ""))
+    formula =  celsius => celsius * 9/5 + 32  ;;
+    degreuSign = "F"
+   }
+   return  viewfinder.innerHTML= formula(degreeu) + degreuSign
     console.log( DateInput)
     
 }
